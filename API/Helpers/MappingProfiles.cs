bb0a751f7b8a17570717a3c472d856de99321fb8
nhannguyen <ngtn0502@gmail.com>
2022-07-07 00:51:26 +0700

    Mapping product data to dto

---
+++ API/Helpers/MappingProfiles.cs
@@ -0,0 +1,17 @@
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+   public class MappingProfiles : Profile
+   {
+      public MappingProfiles()
+      {
+         CreateMap<Product, ProductToReturnDto>()
+            .ForMember(p => p.ProductBrand, o => o.MapFrom(p => p.ProductBrand.Name))
+            .ForMember(p => p.ProductType, o => o.MapFrom(p => p.ProductType.Name))
+            .ForMember(p => p.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
+      }
+   }
+}
---
+++ API/Helpers/ProductUrlResolver.cs
@@ -0,0 +1,20 @@
+using API.Dtos;
+using AutoMapper;
+using Core.Entities;
+
+namespace API.Helpers
+{
+   public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+   {
+      private readonly IConfiguration _config;
+      public ProductUrlResolver(IConfiguration config)
+      {
+         _config = config;
+      }
+
+      public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+      {
+         return _config["AppUrl"] + source.PictureUrl;
+      }
+   }
+}
